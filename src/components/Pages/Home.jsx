7c2b05c59f2dd2773038a39dd0c5fa0e5acd336e
NoNameCoder <johnthetheologian@mail.ru>
2025-01-28 11:08:17 +0300

    slider fix

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,20 +1,44 @@
 import Header from "../Header/Header";
 import { ThemeProvider } from "../ChangeTheme";
 import Footer from "../Footer/Footer";
-import { Carousel } from 'antd';
+import { Carousel } from "antd";
 import Arrow from "../../images/arrow.svg";
 import slider_list from "../../images//Slider/slider_list.png";
 import { useState, useEffect } from "react";
+import styled from 'styled-components';
 
 const contentStyle = {
-  marginBottom: '20px',
-  height: '345px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-  // overflow: 'hidden',
+  marginBottom: "20px",
+  height: "345px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#EDE4CA",
+  
 };
+
+const CarouselWrapper = styled(Carousel)`
+  > .slick-dots li button {
+    width: 20px;
+    height: 20px;
+    opacity: 1;
+    background: #EDE4CA;
+    border-radius: 100%;
+  }
+  > .slick-dots li.slick-active button {
+    width: 20px;
+    height: 20px;
+    background: #DB7611;
+    border-radius: 100%;
+  }
+`;
+
+const CarouselList = [
+  slider_list,
+  slider_list,
+  slider_list,
+  slider_list,  
+]
 
 const Home = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -39,22 +63,15 @@ const Home = () => {
     <ThemeProvider>
       <>
         <Header />
-        <Carousel autoplay style={{marginTop: '30px'}}>
-          <div>
-            <h3 style={contentStyle}>
-              <img src={slider_list} alt="slider list" />
-            </h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>2</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>3</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>4</h3>
-          </div>
-        </Carousel>
+        <CarouselWrapper autoplay style={{ marginTop: "30px" }}>
+          {
+            CarouselList.map((data, obj) => (
+              <div key={obj}>
+                 <img src={data} alt="slider list" style={contentStyle}/>
+              </div>
+            ))
+          }
+        </CarouselWrapper>
         <main>
           <h1>Тут будет титульный текст</h1>
           <h3>Тут будет текст для описания</h3>
